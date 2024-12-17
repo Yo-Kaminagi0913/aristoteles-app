f6669a4c256b6f423f1d9cafe5e4f0cac9aa2800
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-12-17 10:09:25 +0900

    登録・ログイン後にユーザートップへ遷移する設定

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
   def index
     # ログイン済みの場合ログイン後のページにリダイレクト
     if user_signed_in?
-      redirect_to user_path
+      redirect_to user_path(current_user.id)
     else
       # ログインしていない場合ゲスト向けトップページを表示
       render :index
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'homes#index' # トップページをhomesコントローラーのindexアクションに設定
+  resources :users, only: [:show, :edit, :update] # ユーザー情報編集機能のルーティング
 end
