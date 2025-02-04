ca58f52994ec690db1fb9b53479695d2b32f49c0
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2025-02-04 15:44:46 +0900

    StoryQuestionsコントローラーのルーティングを設定
    StoriesのルーティングにネストさせてStoriesとStoryQuestionsを関連付けている

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#index' # トップページをhomesコントローラーのindexアクションに設定
   resources :users, only: [:show, :edit, :update, :destroy] # ユーザー情報編集機能のルーティング
-  resources :stories, only: [:new, :create, :show]
+  resources :stories, only: [:new, :create, :show] do
+    resources :story_questions, only: [:new, :create]
+  end
 end
