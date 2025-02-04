0775131380f3225343c92e79d0b781b916c7f3d7
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2025-02-04 16:18:43 +0900

    質問・回答モデルへのアソシエーション、バリデーション記述

--- app/models/beat_sheet_answer.rb
+++ app/models/beat_sheet_answer.rb
@@ -1,4 +1,6 @@
 class BeatSheetAnswer < ApplicationRecord
   belongs_to :story
   belongs_to :beat_sheet_question
+
+  validates :content, presence: true
 end
--- app/models/beat_sheet_question.rb
+++ app/models/beat_sheet_question.rb
@@ -1,2 +1,5 @@
 class BeatSheetQuestion < ApplicationRecord
+  has_many :beat_sheet_answers, dependent: :destroy
+
+  validates :content, presence: true
 end
