e712c4f1a11f80679b25f89ec68e4f1ec84c1e33
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2025-02-04 15:59:10 +0900

    回答の保存機能を作成

--- app/controllers/story_questions_controller.rb
+++ app/controllers/story_questions_controller.rb
@@ -1,2 +1,21 @@
 class StoryQuestionsController < ApplicationController
+  def new
+    @story = Story.find(params[:story_id])
+    @questions = BeatSheetQuestion.all
+  end
+
+  def create
+    @answer = BeatSheetAnswer.new(answer_params)
+    if @answer.save
+      redirect_to new_story_question_path(@answer.story)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:beat_sheet_answer).permit(:content, :beat_sheet_question_id).merge(story_id: params[:story_id])
+  end
 end
