72b79c332107abe11ce818e67d4d26f3d14145f9
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2025-02-04 15:40:35 +0900

    Storiesコントローラーに処理を追加

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,20 @@
 class StoriesController < ApplicationController
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = current_user.stories.build(story_params)
+    if @story.save
+      redirect_to new_story_question_path(@story)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :genre_id, :medium_id)
+  end
 end
